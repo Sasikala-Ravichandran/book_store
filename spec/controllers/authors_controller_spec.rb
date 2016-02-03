4dd63737fde97bfab7aaf111652615ed346ec2d9
Sasikala-Ravichandran <passion.rails@gmail.com>
2016-02-02 20:31:41 -0500

    Spec for author create successful path

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe AuthorsController, :type => :controller do
   end
 
   describe "Get #show" do
-    let!(:author) { Fabricate(:author) }
+    let(:author) { Fabricate(:author) }
     it "returns a successful show template" do
       get :show, id: author.id
       #require 'pry';binding.pry
@@ -26,4 +26,23 @@ RSpec.describe AuthorsController, :type => :controller do
     end
   end
 
+  describe "Post #create" do
+
+    context "a successful create" do
+      it "saves new author object" do
+        post :create, author: Fabricate.attributes_for(:author)
+        expect(Author.count).to eq(1)
+      end
+
+      it "redirects to show page" do
+        post :create, author: Fabricate.attributes_for(:author)
+        expect(response).to redirect_to author_path(Author.last)
+      end
+
+      it "displays flash message" do
+        post :create, author: Fabricate.attributes_for(:author)
+        expect(flash[:success]).to eq("Author has been created")
+      end
+    end
+  end
 end
