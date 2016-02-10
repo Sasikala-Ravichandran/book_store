e6a152a2ff1fc4eeb61a3b2c698bed210951a86c
Sasikala-Ravichandran <passion.rails@gmail.com>
2016-02-10 13:44:10 -0500

    Implemented catalogs controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
 
   resources :users, only: [:index, :show, :new, :create]
-  
+  resources :catalogs, only: [:index, :show ]
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
