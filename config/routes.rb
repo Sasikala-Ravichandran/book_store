248f3b4cc36ad5526a3de67078f2b575f4eef6ef
Sasikala-Ravichandran <passion.rails@gmail.com>
2016-02-14 10:26:52 -0500

    Implemented orderscontroller

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   end
   resources :carts, only: [:show, :destroy]
   resources :cart_items, only: [:create]
+  resources :orders, only: [:new]
   
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
---
+++ spec/controllers/order_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+require 'support/macro'
+RSpec.describe OrdersController, :type => :controller do
+
+  let(:user) { Fabricate(:user) }
+  before { set_current_user(user) }
+
+  describe "GET #new" do 
+
+    it "returns new template" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+    
+    it "redirects unquthorized user" do
+      clear_current_user 
+      get :new
+      expect(response).to redirect_to(login_path)
+    end
+  end
+end
