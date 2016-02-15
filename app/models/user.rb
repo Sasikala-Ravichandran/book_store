f791d387c719515df601255c7990ef0d3816dd3d
Sasikala-Ravichandran <passion.rails@gmail.com>
2016-02-15 03:27:02 -0500

    Added Random Token to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
 
+  before_create :generate_token
+
   has_secure_password 
 
   VALID_FORMAT =  /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :first_name, :last_name, :email, :password, presence: true
+  validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: { case_sensitive: false },
                     format: { with: VALID_FORMAT}
 
@@ -14,4 +16,14 @@ class User < ActiveRecord::Base
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def to_param
+    token
+  end
+
+  protected
+  def generate_token
+    self.token = SecureRandom.urlsafe_base64
+  end
+
 end
---
+++ db/migrate/20160215074648_add_tokens_to_users.rb
@@ -0,0 +1,9 @@
+class AddTokensToUsers < ActiveRecord::Migration
+  
+    def change
+      add_column :users, :token, :string
+
+      User.all.each { |user| user.update(token: SecureRandom.urlsafe_base64) } 
+
+    end
+end
